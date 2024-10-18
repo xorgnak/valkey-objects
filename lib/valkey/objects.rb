38768361c7bcc23cdc86af49ec6120c15f8f1207
erik <erik@localhost.localdomain>
2024-10-18 05:34:38 -0600

    first commit

---
+++ lib/valkey/objects.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "objects/version"
+
+module Valkey
+  module Objects
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
